17bb8564d265dc76a9400e5f443139220dfd1c3a
HRaj <hraj@gmail.com>
2015-05-30 12:13:43 -0500

    Flow

---
+++ src/com/intro/operators/BitwiseTest.java
@@ -0,0 +1,34 @@
+package com.intro.operators;
+
+public class BitwiseTest {
+
+	public static void main(String args[]) {
+
+		int number = 8; // 0000 1000
+		System.out.println("Original number : " + number);
+
+		// left shifting bytes with 1 position
+		number = number << 1; // should be 16 i.e. 0001 0000
+
+		// equivalent of multiplication of 2
+		System.out.println("value of number after left shift: " + number);
+
+		number = -8;
+		// right shifting bytes with sign 1 position
+		number = number >> 1; // should be 16 i.e. 0001 0000
+
+		// equivalent of division of 2
+		System.out.println("value of number after right shift with sign: "
+				+ number);
+
+		number = -8;
+		// right shifting bytes without sign 1 position
+		number = number >>> 1; // should be 16 i.e. 0001 0000
+
+		// equivalent of division of 2
+		System.out.println("value of number after right shift with sign: "
+				+ number);
+
+	}
+
+}

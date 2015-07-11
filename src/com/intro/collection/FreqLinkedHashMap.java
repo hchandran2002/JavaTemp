c728cf78b8f2059f8778c86d30677bee4e8e983f
HRaj <hraj@gmail.com>
2015-07-11 13:01:01 -0500

    Java Class 7

---
+++ src/com/intro/collection/FreqLinkedHashMap.java
@@ -0,0 +1,19 @@
+package com.intro.collection;
+
+import java.util.*;
+
+// Run in this format : java Freq if it is to be it is up to me to delegate
+public class FreqLinkedHashMap {
+    public static void main(String[] args) {
+        Map<String, Integer> m = new LinkedHashMap<String, Integer>();
+
+        // Initialize frequency table from command line
+        for (String a : args) {
+            Integer freq = m.get(a);
+            m.put(a, (freq == null) ? 1 : freq + 1);
+        }
+
+        System.out.println(m.size() + " distinct words:");
+        System.out.println(m);
+    }
+}

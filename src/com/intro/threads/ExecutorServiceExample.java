c7bf62dd87824f63825f49b470270a7357a86aaa
hchandran2002 <test@test.com>
2015-07-22 01:13:42 -0500

    Java Class 8

---
+++ src/com/intro/threads/ExecutorServiceExample.java
@@ -0,0 +1,21 @@
+package com.intro.threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceExample {
+
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+		executorService.execute(new Runnable() {
+		    public void run() {
+		        System.out.println("Asynchronous task");
+		    }
+		});
+
+		executorService.shutdown();
+
+	}
+
+}
